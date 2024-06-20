c7443c967873c22129bbbec1ec771ccbf2966558
iammhador <mhador123@gmail.com>
2024-06-20 20:04:00 +0600

    Fixed: Authentication issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,11 @@
-// export default function Home() {
-//   return (
-//     <div>
-//       <h4 className="text-2xl text-red-400">HELLO WORLD</h4>
-//     </div>
-//   );
-// }
-// import { UserButton } from "@clerk/nextjs";
-
-// export default function Home() {
-//   return <UserButton />;
-// }
-
 import { Button } from "@/components/ui/button";
+import { SignOutButton, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 export default function Home() {
   return (
     <div>
-      <Button>HELLO NOW!</Button>
+      <UserButton />
     </div>
   );
 }
--- provider/ConvexClientProvider.tsx
+++ provider/ConvexClientProvider.tsx
@@ -1,7 +1,59 @@
+// "use client";
+// import { ConvexProviderWithClerk } from "convex/react-clerk";
+// import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
+// import {
+//   ClerkProvider,
+//   SignInButton,
+//   SignedIn,
+//   SignedOut,
+//   UserButton,
+//   useAuth,
+// } from "@clerk/nextjs";
+// import Loader from "@/components/shared/loader";
+
+// type Props = {
+//   children: React.ReactNode;
+// };
+
+// const CONVEX_URL = process.env.NEXT_PUBLIC_CONVEX_URL || "";
+
+// const convex = new ConvexReactClient(CONVEX_URL);
+
+// const ConvexClientProvider = ({ children }: Props) => {
+//   return (
+//     <ClerkProvider>
+//       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+//         <Authenticated>
+//           <SignedOut>
+//             <SignInButton />
+//           </SignedOut>
+//           <SignedIn>
+//             <UserButton />
+//           </SignedIn>
+//           {children}
+//         </Authenticated>
+//         <AuthLoading>
+//           <Loader />
+//         </AuthLoading>
+//       </ConvexProviderWithClerk>
+//     </ClerkProvider>
+//   );
+// };
+
+// export default ConvexClientProvider;
+
 "use client";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
-import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  useAuth,
+} from "@clerk/nextjs";
 import Loader from "@/components/shared/loader";
 
 type Props = {
@@ -16,11 +68,13 @@ const ConvexClientProvider = ({ children }: Props) => {
   return (
     <ClerkProvider>
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-        {children}
-        {/* <Authenticated>{children}</Authenticated>
+        <SignedIn>{children}</SignedIn>
+        <SignedOut>
+          <RedirectToSignIn />
+        </SignedOut>
         <AuthLoading>
           <Loader />
-        </AuthLoading> */}
+        </AuthLoading>
       </ConvexProviderWithClerk>
     </ClerkProvider>
   );
